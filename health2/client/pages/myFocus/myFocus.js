c57a0535184476b729f97b74e2bec08654c454e5
yazhishaw <946017153@qq.com>
2019-04-24 15:22:48 +0800

    我的关注

--- health2/client/pages/myFocus/myFocus.js
+++ health2/client/pages/myFocus/myFocus.js
@@ -1,5 +1,5 @@
 // pages/myFocus/myFocus.js
-const { getFocusApi } = require('../../api/api.js')
+const { getFocusApi, addFocus } = require('../../api/api.js')
 const app = getApp()
 Page({
 
@@ -8,7 +8,7 @@ Page({
    */
   data: {
     focusList:[],
-    noneImg: "../../images/none.jpg"
+    noneImg: "../../images/none.jpg",
   },
 
   /**
@@ -31,9 +31,39 @@ Page({
     let that = this
     // let friendId = that.data.friendId
     app.http(getFocusApi).then(res => {
+      console.log(res.data)
       that.setData({
         focusList: res.data
       })
+    })
+  },
+  toFocus: function (e) {
+    console.log(111)
+    let that = this
+    const friendId = e.currentTarget.dataset.friendid
+    const avatarUrl = e.currentTarget.dataset.avatarurl
+    const nickName = e.currentTarget.dataset.nickname
+    const focus = e.currentTarget.dataset.focus
+    let data = ''
+    // 取消关注
+    wx.showModal({
+      title: '提示',
+      content: '确定取消关注吗？',
+      success: function (res) {
+        if (res.confirm) {
+          app.http(addFocus, { friendId }, 'POST').then(res => {
+            if (res.code === 200) {
+              app.http(getFocusApi, data).then(res => {
+                that.setData({
+                  focusList: res.data
+                })
+              })
+            }
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
     })
   },
   toOneArticle: function (e) {
